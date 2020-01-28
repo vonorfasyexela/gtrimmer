e7882aabcb548bb444349ddfb1bbdf9b1bac0f00
Alexey Safronov <vonorfasyexela@gmail.com>
2020-01-29 02:58:58 +0300

    Fix issue with a short menubar

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,11 @@ int main(int argc, char *argv[])
     Gstreamer gstreamer(&argc, &argv);
     Window window;
     window.refresh_plugins();
+
+    // fix issue with short menubar
+    QDesktopWidget dw;
+    window.setGeometry(0, 0, dw.width(), dw.height());
     window.showMaximized();
+
     return app.exec();
 }
