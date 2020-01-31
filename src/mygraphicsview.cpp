59e25011b9d4de8f7d93ebd9e7b282f786bc806f
Alexey Safronov <vonorfasyexela@gmail.com>
2020-01-31 19:00:27 +0300

    Add drag and drop feature (not working properly yet)

--- src/mygraphicsview.cpp
+++ src/mygraphicsview.cpp
@@ -1,7 +1,14 @@
+#include <QtWidgets>
+
 #include "mygraphicsview.h"
+#include "pluginitem.h"
 
 void MyGraphicsView::dragEnterEvent(QDragEnterEvent *event)
 {
-    Q_UNUSED(event);
     printf("Drag entered\n");
+    printf("Position rX: %d, rY: %d\n", event->pos().x(), event->pos().y());
+
+    PluginItem *plugin = new PluginItem();
+    this->scene()->addItem(plugin);
+    plugin->setPos(event->pos().x(), event->pos().y());
 }
