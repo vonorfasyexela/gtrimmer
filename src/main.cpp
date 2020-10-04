261656748b5d896408d829fc32367c6659c3c001
Alexey Safronov <vonorfasyexela@gmail.com>
2020-10-05 00:43:56 +0300

    Some cosmetic code fixes

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,16 @@
 
 int main(int argc, char *argv[])
 {
-    // on some platforms resources are placed as static library
-    // and this string is required there
+    // On some platforms resources are placed as static library
+    // and this string is required there.
     Q_INIT_RESOURCE(resources);
 
-    qDebug() << "Start";
     QApplication app(argc, argv);
-//    Gstreamer gstreamer(&argc, &argv);
     Window window(&argc, &argv);
     window.refresh_plugins();
     window.showGstVersion(gst_version_string());
 
-    // fix issue with short menubar
+    // Fix issue with short menubar.
     window.setGeometry(app.screens()[0]->geometry());
     window.showMaximized();
 
